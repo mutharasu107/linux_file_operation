b215e4755bd52eec1e5acec585433dd92f6619c2
MUTHU <81810498+mutharasu107@users.noreply.github.com>
2023-11-29 12:21:14 +0530

    Add files via upload

--- file_permission/permission.c
+++ file_permission/permission.c
@@ -1,3 +1,10 @@
+/***************************************************************************************************************/
+/*   Author         : Mutharsu R                                                                               */
+/*   Date           : 29/11/2023                                                                               */
+/*   File name      : permission.c                                                                             */
+/*   Description    : File Permission Checker                                                                  */
+/***************************************************************************************************************/
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -7,24 +14,28 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char** argv)
 {
+    struct timespec st_mtim;  /* Time of last modification */
    struct stat statbuffer;
    int ret;
    int iter=0;
    char *error_message;
 
-   if(argc < 2) {
+   if(argc < 2) 
+   {
       fprintf(stderr,"please pass a file name\n");
       exit(-1);
    }
 
-
-   for(iter=1; iter<argc; iter++) {
+   for(iter=1; iter<argc; iter++) 
+   {
         char str[11] = "--- --- ---";
    	ret = stat(argv[iter], &statbuffer);
-   	if(ret < 0) {
+   	if(ret < 0) 
+	{
 	        ret = errno;
 		error_message = strerror(errno);
 		fprintf(stderr, "%s: %s\n", argv[iter], error_message);
@@ -40,13 +51,16 @@ int main(int argc, char** argv)
    	str[9] = statbuffer.st_mode & S_IWOTH ? 'w' : '-';
    	str[10] = statbuffer.st_mode & S_IXOTH ? 'x' : '-';
 
-   	printf("\nFile name: %s\n", argv[iter]);
+	printf("\n File_name inode mode num_of_links uid gid size_in_bytes \n");
+	printf("%s %ld %s %ld %d %d %ld\n", argv[iter], statbuffer.st_ino, str, statbuffer.st_nlink, statbuffer.st_uid, statbuffer.st_gid, statbuffer.st_size);
+	printf("Last file modification time:   %s", ctime(&statbuffer.st_mtime));
+/*   	printf("\nFile name: %s\n", argv[iter]);
    	printf("inode %ld\n", statbuffer.st_ino);
    	printf("mode %s\n", str);
    	printf("num of links %ld\n", statbuffer.st_nlink);
    	printf("uid %d\n", statbuffer.st_uid);
    	printf("gid %d\n", statbuffer.st_gid);
-   	printf("size in bytes %ld\n", statbuffer.st_size);
+   	printf("size in bytes %ld\n", statbuffer.st_size);*/
    }
    return EXIT_SUCCESS;
 }
