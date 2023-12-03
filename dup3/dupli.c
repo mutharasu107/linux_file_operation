f637e244c6ad3f63bf5bfd0712d6509b6ad9f9d5
MUTHU <81810498+mutharasu107@users.noreply.github.com>
2023-12-03 10:55:17 +0530

    Add files via upload

---
+++ dup3/dupli.c
@@ -0,0 +1,34 @@
+/***************************************************************************************************************/
+/*   Author         : Mutharsu R                                                                               */
+/*   Date           : 03/12/2023                                                                               */
+/*   File name      : du.c                                                                                     */
+/*   Description    : call dup2() by passing stdout as old fd and an unused fd as new fd                       */
+/***************************************************************************************************************/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+   int d_dup;
+   ssize_t write_size;
+   
+   //  int dup2(int oldfd, int newfd);
+   d_dup = dup2(1, 10);
+   if(d_dup < 0)
+   {
+      perror("dup2 :");
+      exit(-1);
+   }
+   printf("%d\n", d_dup);
+
+   // ssize_t write(int fd, const void *buf, size_t count);
+   write_size = write(d_dup, "hi school of linux\n", 19);
+   if(d_dup < 0)
+   {
+      perror("write:");
+      exit(-2);
+   }
+   exit(0);
+}
